15b394f1dc34c8121cb752fb4452ef68f8f4f0d3
Anguzu Daniel <anguzud7@gmail.com>
2026-01-28 16:21:01 +0300

    Feat: Add public health check endpoint

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/health', (req, res) => res.json({ status: 'ok' }));
+
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/bot', botRoutes);
 app.use('/api/v1/user', userRoutes);
